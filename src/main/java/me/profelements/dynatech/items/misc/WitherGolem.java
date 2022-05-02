996e96942f693a9549bbdb30babf245b069773d4
ybw0014 <i@guizhanss.com>
2022-05-01 23:55:20 -0400

    格式

--- src/main/java/me/profelements/dynatech/items/misc/WitherGolem.java
+++ src/main/java/me/profelements/dynatech/items/misc/WitherGolem.java
@@ -18,12 +18,12 @@ public class WitherGolem extends MultiBlockMachine {
         super(itemGroup, item, new ItemStack[] {null, new ItemStack(Material.CARVED_PUMPKIN), null, null, new ItemStack(Material.POLISHED_BLACKSTONE), null, null, new ItemStack(Material.POLISHED_BLACKSTONE), null}, BlockFace.SELF);
     }
 
-  @Override
-  public void onInteract(@Nonnull Player p, @Nonnull Block b) {
-    Block pumpkinHead = b.getRelative(BlockFace.UP);
-    Block bottomBlackstone = b.getRelative(BlockFace.DOWN);
-  
-    p.getWorld().spawnEntity(b.getLocation().add(0.5, -1, 0.5), EntityType.WITHER_SKELETON);
+    @Override
+    public void onInteract(@Nonnull Player p, @Nonnull Block b) {
+        Block pumpkinHead = b.getRelative(BlockFace.UP);
+        Block bottomBlackstone = b.getRelative(BlockFace.DOWN);
+
+        p.getWorld().spawnEntity(b.getLocation().add(0.5, -1, 0.5), EntityType.WITHER_SKELETON);
 
         pumpkinHead.setType(Material.AIR);
         b.setType(Material.AIR);
